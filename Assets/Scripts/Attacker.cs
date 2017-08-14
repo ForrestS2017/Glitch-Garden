361a9ab9039fae5fa32febdd86803a7e3f6ab00d
Forrest Smith <fsmith2017@gmail.com>
2017-08-14 16:09:28 -0400

    04) Working attackers, health, and death

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,15 +7,20 @@ public class Attacker : MonoBehaviour {
 
     private float WalkSpeed;
     private GameObject currentTarget;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-
+        animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+        if (!currentTarget)
+        {
+            animator.SetBool("isAttacking", false);
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,7 +36,16 @@ public class Attacker : MonoBehaviour {
     // Called from the animator at the time of actual attack
     public void StrikeCurrentTarget(float damage)
     {
-        Debug.Log(name + " attacked for: " + damage);
+        if (currentTarget)
+        {
+            Health health = currentTarget.GetComponent<Health>();
+            if (health)
+            {
+                health.DealDamage(damage);
+            }
+        }
+
+
     }
 
     public void Attack(GameObject obj)
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public float health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+
+        if(health < 0)
+        {
+            DestroySelf();
+        }
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
